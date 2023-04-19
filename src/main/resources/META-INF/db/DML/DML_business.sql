c1fbfa86a3c016748e0c16cfda9084cdc304e9e7
kdkk2000 <119981831+kdkk2000@users.noreply.github.com>
2023-04-19 17:01:46 +0900

    Update DML_business.sql

--- src/main/resources/META-INF/db/DML/DML_business.sql
+++ src/main/resources/META-INF/db/DML/DML_business.sql
@@ -13,24 +13,24 @@ UPDATE Business
 
 -- 3. 업체 등록요청 (등록글 작성)
 -- Hotel 등록
-INSERT INTO Hotel (hName, bId, locationNo, hAddr, hTel, hLink, hInfo, inTime, outTime, inDate, outDate, hMainImg, hSubImg_1, hSubImg_2, hSubImg_3, hLatitude, hLongitude, hPrice, requestStatus)
-    VALUES ('호텔', 'bid', (SELECT locationNo FROM (SELECT * FROM location WHERE lName = '제주시') WHERE ROWNUM = 1), '제주', '010-1234-1324', 'www.naver.com', '정보', '14:00', '11:00', TO_DATE('2023-05-01', 'YYYY-MM-DD'), TO_DATE('2023-05-03', 'YYYY-MM-DD'), 'main.jpg', 'sub_img_1', 'sub_img_2', 'sub_img_3', 50.5, 50.5, '40,000', 'P');
+INSERT INTO Hotel (hName, bId, locationNo, hAddr, hTel, hLink, hInfo, inTime, outTime, hMainImg, hSubImg_1, hSubImg_2, hSubImg_3, hLatitude, hLongitude, hPrice, requestStatus)
+    VALUES ('호텔', 'bid', (SELECT locationNo FROM (SELECT * FROM location WHERE lName = '제주시') WHERE ROWNUM = 1), '제주', '010-1234-1324', 'www.naver.com', '정보', '14:00', '11:00', 'main.jpg', 'sub_img_1', 'sub_img_2', 'sub_img_3', 50.5, 50.5, 40000, 'P');
 -- Restaurant 등록
 INSERT INTO Restaurant (rName, bId, locationNo, rAddr, rTel, rLink, rInfo, rMenu, rMainImg, rSubImg_1, rSubImg_2, rSubImg_3, rLatitude, rLongitude, rPrice, requestStatus)
-SELECT '식당', 'bid', locationNo, '제주', '02-1234-1234', 'www.naver.com', '정보', '올래국수', 'main_img.jpg', 'sub_img_1', 'sub_img_2', 'sub_img_3', 50.5, 50.5, '40,000', 'P'
+    SELECT 'Restaurant', 'bid', locationNo, 'Jeju', '02-1234-1234', 'www.naver.com', 'Information', 'Ollae Noodles', 'main_img.jpg', 'sub_img_1', 'sub_img_2', 'sub_img_3', 50.5, 50.5, 40000, 'P'
     FROM location
-    WHERE lName = '제주시' AND ROWNUM = 1;
+    WHERE lName = 'Jeju City' AND ROWNUM = 1;
     
 -- 4. 업체 등록요청 (등록글 수정)
 -- Hotel 수정
 UPDATE Hotel
-SET hName = '호텔', locationNo = (SELECT locationNo FROM location WHERE lName = '제주시' AND ROWNUM = 1), hAddr = '제주시', hTel = '010-1234-1324', hLink = 'www.naver.com', hInfo = '정보', inTime = '14:00', outTime = '11:00', inDate = TO_DATE('2023-05-01', 'YYYY-MM-DD'), outDate = TO_DATE('2023-05-04', 'YYYY-MM-DD'),
-        hMainImg = 'new_main.jpg', hSubImg_1 = 'new_sub_img_1', hSubImg_2 = 'new_sub_img_2', hSubImg_3 = 'new_sub_img_3', hLatitude = 50.5, hLongitude = 50.5, hPrice = '50,000'
+SET hName = '호텔', locationNo = (SELECT locationNo FROM location WHERE lName = '제주시' AND ROWNUM = 1), hAddr = '제주시', hTel = '010-1234-1324', hLink = 'www.naver.com', hInfo = '정보', inTime = '14:00', outTime = '11:00',
+        hMainImg = 'new_main.jpg', hSubImg_1 = 'new_sub_img_1', hSubImg_2 = 'new_sub_img_2', hSubImg_3 = 'new_sub_img_3', hLatitude = 50.5, hLongitude = 50.5, hPrice = 50000
 WHERE bId = 'bid';
 
 -- Restaurant 수정
 UPDATE Restaurant
-SET rName = '식당', locationNo = (SELECT locationNo FROM location WHERE lName = '제주시' AND ROWNUM = 1), rAddr = '제주시', rTel = '02-1234-1234', rLink = 'www.naver.com', rInfo = '정보', rMenu = '국수', rMainImg = 'main_img.jpg', rSubImg_1 = 'new_sub_img_1', rSubImg_2 = 'new_sub_img_2', rSubImg_3 = 'new_sub_img_3', rLatitude = 50.5, rLongitude = 50.5, rPrice = '50,000'
+SET rName = '식당', locationNo = (SELECT locationNo FROM location WHERE lName = '제주시' AND ROWNUM = 1), rAddr = '제주시', rTel = '02-1234-1234', rLink = 'www.naver.com', rInfo = '정보', rMenu = '국수', rMainImg = 'main_img.jpg', rSubImg_1 = 'new_sub_img_1', rSubImg_2 = 'new_sub_img_2', rSubImg_3 = 'new_sub_img_3', rLatitude = 50.5, rLongitude = 50.5, rPrice = 50000
     WHERE bId = 'bid';
 
 -- 5. 업체 등록요청 (등록글 삭제)
